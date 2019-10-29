0a7e840adb2d028d7c75495fdc42e3282b1150dd
Marcel Koch <kontakt@marcelkoch.net>
2019-10-30 00:54:49 +0100

    introduce basic-ftp and deploy script

---
+++ src/js/deploy.js
@@ -0,0 +1,28 @@
+const ftp = require("basic-ftp")
+
+const ftpPass = process.env.FTP_PASSWORD
+if (!ftpPass) {
+  console.log("env variable 'FTP_PASSWORD' is not set")
+  process.exit(1)
+}
+
+deploy()
+
+async function deploy() {
+    const client = new ftp.Client()
+    try {
+        await client.access({
+            host: "koch.kasserver.com",
+            user: "f0121715",
+            password: ftpPass,
+            secure: true
+        })
+        console.log(await client.list())
+        await client.clearWorkingDir()
+        await client.uploadFromDir("dist")
+    }
+    catch(err) {
+        console.log(err)
+    }
+    client.close()
+}
