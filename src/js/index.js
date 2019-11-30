7b599c4bcf675149ff6f52aaaeae745748912089
Marcel Koch <kontakt@marcelkoch.net>
2019-12-01 00:50:21 +0100

    deal with cookies

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import dialogPolyfill from 'dialog-polyfill/dist/dialog-polyfill.esm'
+import cookie from 'js-cookie'
 
+const CONSENT_COOKIE = 'mtm_consent'
 
 const dialog = document.querySelector('dialog');
 dialogPolyfill.registerDialog(dialog);
@@ -10,3 +12,13 @@ coachingLink.addEventListener('click', function(e) {
   e.preventDefault();
   dialog.showModal();
 }, false);
+
+if (cookie.get(CONSENT_COOKIE)) {
+  console.log('Cookie is already set')
+} else {
+  _paq.push(['rememberConsentGiven', 1000]);
+  console.log('Cookie has just been set')
+}
+
+
+document.cookie = 'mkCookie=jo; expires=Wed, 31 Oct 2020 08:50:17 GMT;path=/';
